4f0868599eb22b99976092f0b6ab4233597d5324
Yelyzaveta Musianovych <eliza.musichka@gmail.com>
2025-05-19 18:12:42 +0200

    add project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,75 @@
+import css from './App.module.css';
+import { useState } from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
+import type { MovieHttpResponse } from '../../types/data';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import ReactPaginate from 'react-paginate';
+
+export default function App() {
+    const [query, setQuery] = useState<string>("");
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+    const {data, isSuccess, isLoading, isError} = useQuery<MovieHttpResponse, Error>({
+        queryKey: ['movies', query, currentPage],
+        queryFn: () => fetchMovies(query, currentPage),
+        enabled: query !== "",
+        placeholderData: keepPreviousData,
+    })
+
+    const totalPages = data?.total_pages ?? 0;
+
+    const handleSubmit = async (newquery: string) => {
+        setQuery(newquery);
+        setCurrentPage(1);
+    };
+
+    const handleSelect = (movie: Movie) => {
+        setSelectedMovie(movie);
+        setIsModalOpen(true);
+    };
+    const handleClose = () => {
+        setIsModalOpen(false);
+        setSelectedMovie(null);
+    };
+
+    return (<div className={css.app}>
+    <SearchBar 
+    onSubmit={handleSubmit} 
+    />
+    {isSuccess && totalPages > 1 && (
+       <ReactPaginate
+        pageCount={totalPages}
+        pageRangeDisplayed={5}
+        marginPagesDisplayed={1}
+        onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+        forcePage={currentPage - 1}
+        containerClassName={css.pagination}
+        activeClassName={css.active}
+        nextLabel="→"
+        previousLabel="←"
+        />
+    )}
+    {isSuccess && data.results.length > 0 && 
+    <MovieGrid 
+    onSelect={handleSelect} 
+    movies={data.results} 
+    />
+    }
+    {isLoading && <Loader />}
+    {isError && <ErrorMessage />}
+    {isModalOpen && selectedMovie && (
+        <MovieModal 
+        movie={selectedMovie} 
+        onClose={handleClose} 
+        />
+    )}
+    </div>)
+}
---
+++ src/services/movieService.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import type { MovieHttpResponse } from "../types/data";
+
+const URL = 'https://api.themoviedb.org/3/search/movie';
+
+export const fetchMovies = async (query: string, page: number): Promise<MovieHttpResponse> => {
+    const response = await axios.get<MovieHttpResponse>(
+        URL, {
+            params: {
+                query,
+                include_adult: false,
+                language: 'en-US',
+                page,
+            },
+            headers: {
+                accept: 'application/json',
+                Authorization: import.meta.env.VITE_TMDB_TOKEN,
+            },
+        });
+    return response.data;
+};
+
+export const imgURL = 'https://image.tmdb.org/t/p/w500'; 
